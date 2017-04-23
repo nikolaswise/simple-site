b30b6f1ff029b394cefdcdf0efdd4c9bf0785b0a
Nikolas Wise <nikolaswise@gmail.com>
2017-04-23 12:19:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('I Am Javascript');
+
+// create a thing called 'foo'
+// 'foo' is a function that does stuff
+// it takes no inputs
+var foo = function () {
+  //it does this stuff
+  
+  //create a new thing
+  console.log(document)
+} 
